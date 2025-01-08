e6ddd3ae2f5717be23584a8982ebc43c22339f2f
FabricioWashington <fabriciowashington2015@gmail.com>
2025-01-08 19:10:53 -0300

    incluindo botoes e rota em caixa

--- frontend/src/app/caixa/caixa-routing.module.ts
+++ frontend/src/app/caixa/caixa-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CaixaComponent } from './caixa/caixa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   {
+      path: '',
+      component: CaixaComponent
+    },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- frontend/src/app/caixa/caixa/caixa.component.ts
+++ frontend/src/app/caixa/caixa/caixa.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-caixa',
   standalone: false,
-  
+
   templateUrl: './caixa.component.html',
   styleUrl: './caixa.component.scss'
 })
 export class CaixaComponent {
+  constructor(private router: Router) {}
 
+  navigateTo(path: string): void {
+    this.router.navigate([path]);
+  }
 }
