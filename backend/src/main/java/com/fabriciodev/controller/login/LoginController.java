224a93fa35125bb333dc98af9fa76aec2a4fe953
FabricioWashington <fabriciowashington2015@gmail.com>
2025-01-08 23:18:58 -0300

    removendo importações não usadas

--- backend/src/main/java/com/fabriciodev/controller/login/LoginController.java
+++ backend/src/main/java/com/fabriciodev/controller/login/LoginController.java
@@ -4,7 +4,6 @@ import com.fabriciodev.model.login.Login;
 import com.fabriciodev.service.login.LoginService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
