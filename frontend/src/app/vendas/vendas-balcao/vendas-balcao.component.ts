20406bfe36cc164c2cbcbf9cae9e76e750ad015a
FabricioWashington <fabriciowashington2015@gmail.com>
2025-02-05 22:54:27 -0300

    ajustes no dark-mode

--- frontend/src/app/vendas/vendas-balcao/vendas-balcao.component.ts
+++ frontend/src/app/vendas/vendas-balcao/vendas-balcao.component.ts
@@ -35,32 +35,32 @@ export class VendasBalcaoComponent {
     });
   }
 
-  onPesquisarProduto(event: Event): void {
-    const input = (event.target as HTMLInputElement).value;
+  // onPesquisarProduto(event: Event): void {
+  //   const input = (event.target as HTMLInputElement).value;
 
-    if (input.toLowerCase() === 'produto1') {
-      this.produtoSelecionado = {
-        nome: 'Produto 1',
-        estoque: 10,
-        valor: 'R$ 50,00',
-      };
+  //   if (input.toLowerCase() === 'produto1') {
+  //     this.produtoSelecionado = {
+  //       nome: 'Produto 1',
+  //       estoque: 10,
+  //       valor: 'R$ 50,00',
+  //     };
 
-      this.adicionarProduto();
-    }
-  }
+  //     this.adicionarProduto();
+  //   }
+  // }
 
-  adicionarProduto(): void {
-    const produto: Produto = {
-      item: this.produtosSelecionados.length + 1,
-      codigo: '123456',
-      produto: this.produtoSelecionado.nome,
-      quantidade: this.quantidade,
-      valorUnitario: parseFloat(this.produtoSelecionado.valor.replace('R$', '').replace(',', '.').trim()),
-      valorTotal: this.quantidade * parseFloat(this.produtoSelecionado.valor.replace('R$', '').replace(',', '.').trim()),
-    };
+  // adicionarProduto(): void {
+  //   const produto: Produto = {
+  //     item: this.produtosSelecionados.length + 1,
+  //     codigo: '123456',
+  //     produto: this.produtoSelecionado.nome,
+  //     quantidade: this.quantidade,
+  //     valorUnitario: parseFloat(this.produtoSelecionado.valor.replace('R$', '').replace(',', '.').trim()),
+  //     valorTotal: this.quantidade * parseFloat(this.produtoSelecionado.valor.replace('R$', '').replace(',', '.').trim()),
+  //   };
 
-    this.produtosSelecionados.push(produto);
-  }
+  //   this.produtosSelecionados.push(produto);
+  // }
 
   exibirModalAtalhos(): void {
     console.log('Abrindo modal de atalhos');
